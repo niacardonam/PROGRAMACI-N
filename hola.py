2294d543e545a4c32c166c4213514205ea8e1d57
Estefania <estefania.cardona2@udea.edu.co>
2025-02-05 11:45:37 -0500

    holaaaaa

---
+++ hola.py
@@ -0,0 +1,16 @@
+usuarios=[]
+
+while True:
+    nombre = input("digita un nombre o x:")
+    
+    if nombre == "x":
+        break
+
+    usuarios.append(nombre)
+print("usuarios registrados:", usuarios)
+
+
+#tiene varios nombres repetidos
+nombre1 = {"diego","camila","luisa","raul"}
+nombre2 = {"diego","camila","cesar","david"}
+print(nombre1.intersection(nombre2))
