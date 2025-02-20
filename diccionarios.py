427ba55184b0f49b19b07d6b4dd711c177270ba8
Estefania <estefania.cardona2@udea.edu.co>
2025-02-20 10:36:47 -0500

    hola parcero

---
+++ diccionarios.py
@@ -0,0 +1,10 @@
+diccionario = {
+    "nombre": "estefania",
+    "apellido":"cardona",
+    "edad": 21,
+    "color favorito": "negro"
+}
+
+print(diccionario.get("edad","no encontrado"))
+
+
---
+++ tipos-de-datos.py
@@ -0,0 +1,10 @@
+"string"
+'string'
+
+"""
+tus datos son
+"""
+int #enteros 1,2,3
+float #flotantes 1.2 , 3.5
+True #boolenano
+False #booleano
